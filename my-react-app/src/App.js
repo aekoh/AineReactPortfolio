c1b38d479af0779f667e410d9bcc733b88eb3947
aine <aineekohh@yahoo.com>
2019-06-27 18:08:54 -0500

    I have added a function named About within the AboutJS file

--- my-react-app/src/App.js
+++ my-react-app/src/App.js
@@ -2,13 +2,14 @@ import React from 'react';
 import './App.css';
 import Navv from './components/Navv/Navv';
 import Main from './components/Main/Main';
-
+import About from './components/About/About'
 
 function App() {
   return (
     <div>
       <Navv />
       <Main />
+      <About />
     </div>
     
   );
---
+++ my-react-app/src/components/About/About.js
@@ -0,0 +1,31 @@
+import React from "react";
+import './About.css';
+
+
+function About() {
+    return (
+        <div class="page-section" id="about">
+        <div class="container" id="aboutMe">
+          <h3>About Me</h3>
+          <p>
+            I am a Software Developer working In Austin, Texas. I have spent
+            2 years in the Technology field. My skillset includes but is not
+            limited to React.JS, Node.Js JavaScript and Sequelize. I am also
+            Familiar with the Github and Heroku. 
+          </p>
+          <p>
+            In December 2019, I graduated from Texas Tech University then 
+            Immedietly enrolled into the UT Austin Tech Boot Camp. My knowledge
+            of Fullstack Development has grown since and continues to grow.
+            This challenge as well as my education from Texas Tech has 
+            rounded me to be a reliable asset to your company. 
+          </p>
+          <p>
+            I am currently looking for growth opportunities in Front-End or
+            Full-Stack roles. I would love for you to review my work below.
+          </p>
+        </div>
+      </div>    
+    )
+    }
+export default About
