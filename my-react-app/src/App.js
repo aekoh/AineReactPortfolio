5d86c6310f8db51551d5415e97c25213d03f7043
aine <aineekohh@yahoo.com>
2019-06-27 17:52:06 -0500

    have been so excited about using react i havent been commiting but that will change today so far ive made 3 components and with files within now i will begin working on the About Compnent

---
+++ my-react-app/src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './App.css';
+import Navv from './components/Navv/Navv';
+import Main from './components/Main/Main';
+
+
+function App() {
+  return (
+    <div>
+      <Navv />
+      <Main />
+    </div>
+    
+  );
+}
+
+export default App;
---
+++ my-react-app/src/components/Main/Main.js
@@ -0,0 +1,32 @@
+import React from "react";
+//import Container from 'react-bootstrap/Container';
+//import Row from 'react-bootstrap/Row';
+//import Col from 'react-bootstrap/Col';
+
+import './Main.css'
+
+
+// This code will stack the column on mobile devices by making my imae full width
+
+function Main() {
+    return (
+        
+        <header class="masthead">
+        <div class="container h-100">
+          <div
+            class="row h-100 align-items-center justify-content-center text-right"
+          >
+            <div class="col-lg-10 align-self-end">
+              <h1 class="text-uppercase text-white font-weight-bold" id="name">
+                Aine Ekoh
+              </h1>
+              <hr class="divider my-4" id="mastheadBreak" />
+            </div>
+          </div>
+        </div>
+      </header>
+
+           
+    )
+    }
+export default Main 
