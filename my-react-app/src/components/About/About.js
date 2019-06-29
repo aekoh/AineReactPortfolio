3bdb6c00f8a6a34d9b32a52876c4f28033e61c8a
aine <aineekohh@yahoo.com>
2019-06-28 21:21:56 -0500

    added links to portfolio js

--- my-react-app/src/components/About/About.js
+++ my-react-app/src/components/About/About.js
@@ -9,16 +9,17 @@ function About() {
           <h3 class="text-white font-weight-bold">About Me</h3>
           <p className="pStyle" class="text-white font-weight-bold">
             I am a Software Developer working In Austin, Texas. I have spent
-            1-2 years in the Technology field. My skillset includes but is not
+            a couple of years in the technology field. My skillset includes but is not
             limited to React.JS, Node.Js JavaScript and Sequelize. I am also
             familiar with Github and Heroku. 
           </p>
           <p class="text-white font-weight-bold">
             In December 2019, I graduated from Texas Tech University then 
-            Immedietly enrolled into the UT Austin Tech Boot Camp. My knowledge
-            of Fullstack Development has grown since and continues to grow.
-            This challenge as well as my education from Texas Tech has 
-            rounded me to be a reliable asset to your company. 
+            immedietly enrolled into the UT Austin Tech Boot Camp. My knowledge
+            of Fullstack Development has grown since my enrollment in this bootcamp.
+            i have gained knowledge that i believe will make me an asset to your company 
+            through this program as well as my prior education from Texas Tech 
+            University.
           </p>
           <p class="text-white font-weight-bold">
             I am currently looking for growth opportunities in Front-End or
--- my-react-app/src/components/Portfolio/Portfolio.js
+++ my-react-app/src/components/Portfolio/Portfolio.js
@@ -12,7 +12,7 @@ import Button from 'react-bootstrap/Button';
 function Portfolio() {
     return (
 
-        <CardDeck>
+        <CardDeck id='cardDeck'>
   <Card id='reliveCard'>
     <Card.Body>
      <Button id='reliveButton'> <Card.Title><a id='reliveColor' class="static-link" href="https://lamaprojects.github.io/project1/" >*reLive App</a></Card.Title></Button>
@@ -30,23 +30,18 @@ function Portfolio() {
     <Card.Body>
      <Button id='nodeButton'> <Card.Title><a id='nodeColor' class="static-link" href="https://friend-finder-aine.herokuapp.com/" >*Node App</a></Card.Title></Button>
       <Card.Text>
-      A Language Interpretation and Recognition Interface command line.
-       A command line node app that takes in parameters and gives back 
-       data to users.
+      Uses Node Js and Express to build a Friend finder application.
       </Card.Text>
     </Card.Body>
     <Card.Footer id='nodeFooter'>
      
     </Card.Footer>
   </Card>
-  <Card>
-    <Card.Img variant="top" src="holder.js/100px160" />
+  <Card id='reactCard'>
     <Card.Body>
-      <Card.Title>Card title</Card.Title>
+    <Button id='reactButton'> <Card.Title><a id='nodeColor' class="static-link" href="https://friend-finder-aine.herokuapp.com/" >*React App</a></Card.Title></Button>
       <Card.Text>
-        This is a wider card with supporting text below as a natural lead-in to
-        additional content. This card has even longer content than the first to
-        show that equal height action.
+       Mapping Tool
       </Card.Text>
     </Card.Body>
     <Card.Footer>
