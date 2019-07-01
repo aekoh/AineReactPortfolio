5327b03eb500d77f29f3d99eb3e1cb76f842884f
aine <aineekohh@yahoo.com>
2019-06-30 19:06:03 -0500

    links navigate to ID sections on page

--- src/components/Navv/Navv.js
+++ src/components/Navv/Navv.js
@@ -1,33 +1,23 @@
 import React from "react";
 import Navbar from 'react-bootstrap/Navbar';
 import Nav from 'react-bootstrap/Nav';
-import NavDropdown from 'react-bootstrap/NavDropdown';
+
 import './Navv.css'
 
 
 function Navv() {
     return (
-<Navbar collapseOnSelect expand="lg" bg="dark" variant="dark" className="navColor">
+<Navbar collapseOnSelect expand="lg" bg="dark" id="" variant="dark" className="navColor">
   <Navbar.Brand href="#home">Aine Ekoh</Navbar.Brand>
   <Navbar.Toggle aria-controls="responsive-navbar-nav" />
   <Navbar.Collapse id="responsive-navbar-nav">
     <Nav className="mr-auto">
-      <Nav.Link href="#features">Features</Nav.Link>
-      <Nav.Link href="#pricing">Pricing</Nav.Link>
-      <NavDropdown title="Dropdown" id="collasible-nav-dropdown">
-        <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-        <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
-        <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-        <NavDropdown.Divider />
-        <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
-      </NavDropdown>
+    <Nav.Link href="#aboutMe">About </Nav.Link>
+      <Nav.Link href="#cardDeck">Portfolio </Nav.Link>
+      <Nav.Link href="#skills">Skillset</Nav.Link>
+     <Nav.Link href="#contact">Contact</Nav.Link>
     </Nav>
-    <Nav>
-      <Nav.Link href="#deets">More deets</Nav.Link>
-      <Nav.Link eventKey={2} href="#memes">
-        Dank memes
-      </Nav.Link>
-    </Nav>
+    
   </Navbar.Collapse>
 </Navbar>
     )
