19a52b61a5af54707ff5742469c02facce1f3051
aine <aineekohh@yahoo.com>
2019-07-11 12:59:21 -0500

    updated about me

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -13,20 +13,17 @@ function About() {
           <p className="pStyle font-weight-bold">
             I am a Software Developer working In Austin, Texas. I have spent
             a couple of years in the technology field. My skillset includes but is not
-            limited to React.JS, Node.Js JavaScript and Sequelize. I am also
+            limited to React.JS, Node.Js Vanilla JavaScript, AJAX, API's, HTML, CSS and Sequelize. I am also
             familiar with Github and Heroku. 
           </p>
           <p className=" font-weight-bold" id='p2'>
-            In December 2018, I graduated from Texas Tech University then 
-            immediately enrolled into the UT Austin Tech Boot Camp. My knowledge
-            of Fullstack Development has grown since my enrollment in this bootcamp.
-            I have gained knowledge that i believe will make me an asset to your company 
-            through this program as well as my prior education from Texas Tech 
-            University.
+            I graduated and completed my undergrad from Texas Tech University. I enrolled
+            in a proffesional post grad program with hands on experience at the
+            University of Texas-Austin Tech Boot Camp. 
           </p>
           <p className=" font-weight-bold" id='p3'>
-            I am currently looking for growth opportunities in Front-End or
-            Full-Stack roles. I would love for you to review my work below.
+            I am currently looking for growth opportunities in Software Development roles
+            My work is yours for review below.
           </p>
         </div>
       </div>    
